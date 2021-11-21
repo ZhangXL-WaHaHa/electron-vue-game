c0105e0382b150445e73418465166a790c677f01
ZhangXL-WaHaHa <2263378161@qq.com>
2021-11-21 13:35:38 +0800

    first commit

---
+++ dist/static/js/chunk-2d0bdf39.79e14ca9.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bdf39"],{"2dd5":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("请用电脑浏览器打开")])},c=[],l={name:"Web"},a=l,d=t("2877"),r=Object(d["a"])(a,u,c,!1,null,null,null);e["default"]=r.exports}}]);
